291016ad7d04ffb82d047aa225a03a61c6d1dfdc
Robert Richter <robert.c.richter@gmail.com>
2017-12-28 10:17:07 -0600

    Check remote peer ID against local peer ID

--- lib/effusion/peer_connection.ex
+++ lib/effusion/peer_connection.ex
@@ -20,11 +20,22 @@ defmodule Effusion.PeerConnection do
   defp handle_handshake(socket) do
     with {:ok, data} <- :gen_tcp.recv(socket, 0),
          {:ok, peer_id, info_hash, _reserved} <- Handshake.decode(data),
-         :ok = Torrents.lookup(info_hash)
+         :ok = Torrents.lookup(info_hash),
+         :ok = check_peer_id(LocalPeer.peer_id(), peer_id)
     do
-      :ok = Logger.info ("Handshake from peer_id #{inspect(peer_id)} for info_hash #{inspect(info_hash)}")
-      response = Handshake.encode(info_hash, LocalPeer.peer_id())
+      :ok = Logger.info ("Handshake from peer_id #{Base.encode16(peer_id)} for info_hash #{Base.encode16(info_hash)}")
+      response = Handshake.encode( LocalPeer.peer_id(), info_hash)
       :ok = :gen_tcp.send(socket, response)
+      :ok
+    else
+      err -> err
+    end
+  end
+
+  defp check_peer_id(local_peer_id, remote_peer_id) do
+    if(local_peer_id == remote_peer_id) do
+      {:error, :remote_same_as_local}
+    else
       :ok
     end
   end
--- test/effusion/peer_connection_test.exs
+++ test/effusion/peer_connection_test.exs
@@ -1,7 +1,14 @@
+alias Effusion.Messages.Handshake
+
 defmodule Effusion.PeerConnectionTest do
   use ExUnit.Case
   doctest Effusion.PeerConnection
   @moduletag :capture_log
+
+  @local_peer_id <<0 :: size(160)>>
+  @remote_peer_id <<1 :: size(160)>>
+  @good_info_hash <<0 :: size(160)>>
+  @bad_info_hash <<1 :: size(160)>>
 
   setup do
     opts = [:binary, packet: 0, active: false]
@@ -9,44 +16,41 @@ defmodule Effusion.PeerConnectionTest do
     %{socket: socket}
   end
 
-  test "server interaction", %{socket: socket} do
-    assert send_and_recv(socket, handshake()) == handshake()
+  test "responds with handshake", %{socket: socket} do
+    request = Handshake.encode(@good_info_hash, @remote_peer_id)
+    response = Handshake.encode(@good_info_hash, @local_peer_id)
+
+    assert send_and_recv(socket, request) == response
   end
 
   test "closes connection when protocol name is wrong", %{socket: socket} do
-    bad_name_size = <<22>> ## should be 19!
-    name = "BitTorrent protocol v2"
-    reserved = <<0 :: size(64)>>
-    info_hash = <<0 :: size(160)>>
-    peer_id = <<0 :: size(160)>>
+    bad_request = bad_handshake(@good_info_hash, @remote_peer_id)
 
-    bad_handshake = bad_name_size <> name <> reserved <> info_hash <> peer_id
-
-    :ok = :gen_tcp.send(socket, bad_handshake)
+    :ok = :gen_tcp.send(socket, bad_request)
     {:error, :closed} = :gen_tcp.recv(socket, 0, 1000)
   end
 
   test "closes connection when info hash isn't recognized", %{socket: socket} do
-    bad_name_size = <<19>>
-    name = "BitTorrent protocol"
-    reserved = <<0 :: size(64)>>
-    info_hash = <<1 :: size(160)>>
-    peer_id = <<0 :: size(160)>>
+    bad_request = Handshake.encode(@bad_info_hash, @remote_peer_id)
 
-    bad_handshake = bad_name_size <> name <> reserved <> info_hash <> peer_id
-
-    :ok = :gen_tcp.send(socket, bad_handshake)
+    :ok = :gen_tcp.send(socket, bad_request)
     {:error, :closed} = :gen_tcp.recv(socket, 0, 1000)
   end
 
-  defp handshake do
-    name_size = <<19>>
-    name = "BitTorrent protocol"
-    reserved = <<0 :: size(64)>>
-    info_hash = <<0 :: size(160)>>
-    peer_id = <<0 :: size(160)>>
 
-    name_size <> name <> reserved <> info_hash <> peer_id
+    test "closes connection when peer ID matches our own", %{socket: socket} do
+      bad_request = Handshake.encode(@good_info_hash, @local_peer_id)
+
+      :ok = :gen_tcp.send(socket, bad_request)
+      {:error, :closed} = :gen_tcp.recv(socket, 0, 1000)
+    end
+
+  defp bad_handshake(info_hash, peer_id) do
+    bad_name_size = <<22>> ## should be 19!
+    name = "BitTorrent protocol v2"
+    reserved = <<0 :: size(64)>>
+
+    bad_name_size <> name <> reserved <> info_hash <> peer_id
   end
 
   defp send_and_recv(socket, text) do
