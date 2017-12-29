768f7dd9784d4860a8dd3d67d660ffd7b8e659e6
Robert Richter <robert.c.richter@gmail.com>
2017-12-29 09:45:52 -0600

    Add some missing test files

---
+++ test/effusion/info_hash_test.exs
@@ -0,0 +1,4 @@
+defmodule Effusion.InfoHashTest do
+  use ExUnit.Case
+  doctest Effusion.InfoHash
+end
---
+++ test/effusion/peer_id_test.exs
@@ -0,0 +1,4 @@
+defmodule Effusion.PeerIdTest do
+  use ExUnit.Case
+  doctest Effusion.PeerId
+end
