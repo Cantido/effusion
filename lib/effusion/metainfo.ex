7892e57795b39bedc03753b936f668601730d6b4
Rosa Richter <rosa.richter@protonmail.com>
2018-01-19 11:19:42 -0600

    Rename metainfo map keys to tokens
    Makes it a little easier to use

--- lib/effusion/metainfo.ex
+++ lib/effusion/metainfo.ex
@@ -5,6 +5,33 @@ defmodule Effusion.Metainfo do
     {:ok, info} = ExBencode.encode(decoded["info"])
     info_hash = :crypto.hash(:sha, info)
 
-    {:ok, Map.put(decoded, :info_hash, info_hash)}
+    result = decoded
+      |> Map.put(:info_hash, info_hash)
+      |> rename_keys(key_tokens())
+      |> Map.update!(:info, &rename_keys(&1, info_tokens()))
+
+    {:ok, result}
+  end
+
+  defp key_tokens do
+    %{
+      "announce" => :announce,
+      "created by" => :created_by,
+      "creation date" => :creation_date,
+      "encoding" => :encoding,
+      "info" => :info
+    }
+  end
+
+  defp info_tokens do
+    %{
+      "length" => :length,
+      "name" => :name,
+      "piece length" => :piece_length
+    }
+  end
+
+  defp rename_keys(map, names) do
+    for {key, val} <- map, into: %{}, do: {Map.get(names, key, key), val}
   end
 end
