53c741f817daedd5568a387416ba475ce1ffc6d7
Rosa Richter <rosa.richter@protonmail.com>
2018-01-19 11:06:58 -0600

    Successfully complete a handshake
    Obviously this is not Finished but I want to check this in
    just so I have a working state to go back to.

--- lib/effusion/session.ex
+++ lib/effusion/session.ex
@@ -1,6 +1,7 @@
 defmodule Effusion.Session do
   alias Effusion.Metainfo
   alias Effusion.LocalPeer
+  alias Effusion.PWP.Messages.Handshake
 
   def start(meta_bin) when is_binary(meta_bin) do
     {:ok, meta} = Metainfo.decode(meta_bin)
@@ -18,6 +19,25 @@ defmodule Effusion.Session do
     http_res = HTTPotion.get("http://localhost:6969?" <> URI.encode_query(tracker_request))
     {:ok, res} = ExBencode.decode(http_res.body)
 
+    peer = select_peer(res["peers"], LocalPeer.peer_id())
 
+    opts = [packet: 0, packet_size: 68, active: false]
+
+    {:ok, peer_ip} = :inet.parse_address to_charlist(peer["ip"])
+    peer_port = peer["port"]
+
+    IO.puts "trying to connect to IP #{inspect(peer_ip)}"
+    IO.puts "port: #{inspect(peer_port)}"
+
+    {:ok, socket} = :gen_tcp.connect(peer_ip, peer_port, opts)
+    :ok = :gen_tcp.send(socket, Handshake.encode(LocalPeer.peer_id(), meta.info_hash))
+    {:ok, handshake } = :gen_tcp.recv(socket, 68, 5000)
+
+    IO.puts "I got a handshake!!"
+  end
+
+  def select_peer(peers, peer_id) do
+    IO.puts "Peers to choose from: #{inspect(peers, pretty: true)}"
+    peers |> Enum.find(fn(p) -> p["peer_id"] != peer_id end)
   end
 end
