674ebae57d820969d6281eb53407e1c82589f8cb
Bobby Richter <robert.c.richter@gmail.com>
2018-01-21 14:34:07 -0600

    Stop testing for a registry I deleted

--- test/effusion_test.exs
+++ test/effusion_test.exs
@@ -1,11 +1,4 @@
 defmodule EffusionTest do
   use ExUnit.Case
   doctest Effusion
-
-  test "starts a torrent registry" do
-    info_hash = <<1 :: size(160)>>
-
-    Registry.register(Effusion.TorrentRegistry, info_hash, :ok)
-    assert [{self(), :ok}] == Registry.lookup(Effusion.TorrentRegistry, info_hash)
-  end
 end
