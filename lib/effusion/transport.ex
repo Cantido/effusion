3dc9c8342cfb257ee2af8aa91aba995df1384d67
Bobby Richter <robert.c.richter@gmail.com>
2018-01-23 11:56:36 -0600

    Define a behavior for a transport layer
    This serves two uses:
    
    1. We can mock out TCP when we test our PWP implementation
    2. We can swap out a TCP implementation for a future uTP implementation
    
    We should keep this behavior compatible with gen_tcp, but we can always
    write a shim if we need to.

---
+++ lib/effusion/transport.ex
@@ -0,0 +1,8 @@
+defmodule Effusion.Transport do
+  @opaque socket :: any | :inet.socket
+
+  @type address :: :inet.socket_address | :inet.hostname
+
+  @callback connect(address, :inet.port_number, [any]) :: {:ok, socket} | {:error, any}
+  @callback recv(socket, non_neg_integer) :: {:ok, String.t | binary} | {:error, any()}
+end
