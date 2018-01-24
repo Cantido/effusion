bc409b28601c6f89c9b9caaa283c59f6170639be
Rosa Richter <rosa.richter@protonmail.com>
2018-01-24 11:58:51 -0600

    Use a GenServer instead of a Connection
    This is easier, and GenServer is actually maintained :V

--- lib/effusion/pwp/peer.ex
+++ lib/effusion/pwp/peer.ex
@@ -1,36 +1,28 @@
 defmodule Effusion.PWP.Peer do
-  use Connection
+  use GenServer
 
   @transport Application.get_env(:effusion, :peer_transport)
 
   def start_link([host, port, peer_id, info_hash]) do
-    Connection.start_link(__MODULE__, [{host, port}, peer_id, info_hash])
+    GenServer.start_link(__MODULE__, [host, port, peer_id, info_hash])
   end
 
-  def await(pid) do
-    Connection.call(pid, :await)
-  end
-
-# [ip, port, peer_id, info_hash]
-  def init([{host, port}, peer_id, info_hash]) do
+  def init([host, port, peer_id, info_hash]) do
     {
-      :connect,
-      {host, port},
-      %{peer_id: peer_id, info_hash: info_hash}
+      :ok,
+      %{
+        host: host,
+        port: port,
+        peer_id: peer_id,
+        info_hash: info_hash
+      },
+      0
     }
   end
 
-  def connect({host, port}, state) do
-    {:ok, socket} = @transport.connect(host, port, [])
-    {:ok, :unchoke} = recv(socket)
-    {:ok, state}
-  end
-
-  def recv(socket) do
-    @transport.recv(socket, 0)
-  end
-
-  def handle_call(:await, _from, state) do
-    {:reply, :ok, state}
+  def handle_info(:timeout, state) do
+    {:ok, socket} = @transport.connect(state.host, state.port, [])
+    {:ok, :unchoke} = @transport.recv(socket, 0)
+    {:noreply, Map.put(state, :socket, socket)}
   end
 end
