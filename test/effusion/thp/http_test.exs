01cbfacb43f5ad8c7f6ff857414c2629d72313b7
Rosa Richter <rosa.richter@protonmail.com>
2018-01-25 09:41:55 -0600

    split HTTP test into multiple sections

--- test/effusion/thp/http_test.exs
+++ test/effusion/thp/http_test.exs
@@ -5,21 +5,37 @@ defmodule Effusion.THP.HTTPTest do
 
   @body File.read! "test/tracker_response.txt"
 
-  test "decodes" do
+  test "decodes interval" do
     {:ok, body} = HTTP.decode @body
 
     assert Map.has_key?(body, :interval)
+
+    assert is_integer(body.interval)
+    assert body.interval > 0
+  end
+
+  test "decodes peers" do
+    {:ok, body} = HTTP.decode @body
     assert Map.has_key?(body, :peers)
 
     [peer | _rest] = body.peers
 
     assert peer != nil
+  end
 
+  test "decodes peer port" do
+    {:ok, body} = HTTP.decode @body
 
-    assert Map.has_key?(peer, :peer_id)
+    [peer | _rest] = body.peers
 
     assert Map.has_key?(peer, :port)
     assert is_integer(peer.port)
+  end
+
+  test "decodes peer ip address" do
+    {:ok, body} = HTTP.decode @body
+
+    [peer | _rest] = body.peers
 
     assert Map.has_key?(peer, :ip)
     assert {a, b, c, d} = peer.ip
@@ -29,4 +45,11 @@ defmodule Effusion.THP.HTTPTest do
     assert is_integer(d)
   end
 
+  test "decodes peer id" do
+    {:ok, body} = HTTP.decode @body
+
+    [peer | _rest] = body.peers
+
+    assert Map.has_key?(peer, :peer_id)
+  end
 end
