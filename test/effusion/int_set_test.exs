75581e1fc898bd260e82c559df702bab4003936c
Rosa Richter <robert.c.richter@gmail.com>
2018-02-01 13:33:22 -0600

    Add some tests

--- test/effusion/int_set_test.exs
+++ test/effusion/int_set_test.exs
@@ -1,6 +1,13 @@
 defmodule Effusion.IntSetTest do
   use ExUnit.Case
+  alias Effusion.IntSet
   doctest Effusion.IntSet
+
+  test "member? returns false if IntSet is too small to contain x" do
+    set = IntSet.new(<<0b0000_0000>>)
+
+    refute IntSet.member?(set, 8)
+  end
 
   test "put can insert values smaller than the max" do
     set = IntSet.new(<<0b0000_0001>>)
@@ -14,5 +21,12 @@ defmodule Effusion.IntSetTest do
        |> IntSet.put(1)
 
     assert IntSet.member?(set, 1)
+  end
+
+  test "put can expand the bitstring" do
+    set = IntSet.new()
+       |> IntSet.put(7)
+
+    assert IntSet.member?(set, 7)
   end
 end
