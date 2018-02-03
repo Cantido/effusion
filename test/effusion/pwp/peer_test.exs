a79385b5fdc51173bd9dc21200436e48f3c5ceb4
Rosa Richter <robert.c.richter@gmail.com>
2018-02-03 17:59:22 -0600

    Get a working PWP handshake test

--- test/effusion/pwp/peer_test.exs
+++ test/effusion/pwp/peer_test.exs
@@ -6,7 +6,8 @@ defmodule Effusion.PWP.PeerTest do
   doctest Effusion.PWP.Peer
 
   @remote_host {127, 0, 0, 1}
-  @remote_port 4040
+  @port 5679
+  @remote_address {@remote_host, @port}
   @local_peer_id "Effusion Experiment!"
   @remote_peer_id "Other peer 123456789"
   @info_hash TestHelper.mint_info_hash()
@@ -14,20 +15,32 @@ defmodule Effusion.PWP.PeerTest do
   @local_handshake Handshake.encode(@local_peer_id, @info_hash)
   @remote_handshake Handshake.encode(@remote_peer_id, @info_hash)
 
-  test "pwp" do
-    port = 5679
-    {:ok, lsock} = :gen_tcp.listen(port, active: false)
-    {:ok, conn} = Peer.connect({{127, 0, 0, 1}, port}, "Effusion Experiment!", <<0::160>>)
+  setup do
+    {:ok, lsock} = :gen_tcp.listen(@port, active: false, reuseaddr: true, send_timeout: 5_000)
 
-    {:ok, csock} = :gen_tcp.accept(lsock, 5_000)
+    on_exit fn ->
+      :ok = :gen_tcp.close(lsock)
+    end
 
-    hs_bin = with {:ok, hsreq} <- :gen_tcp.recv(csock, 68)
-                    do IO.iodata_to_binary(hsreq)
-                    end
+    %{lsock: lsock}
+  end
 
-    {:ok, {"Effusion Experiment!", <<0::160>>, _}} = Handshake.decode(hs_bin)
+  test "Peer performs handshake", %{lsock: lsock} do
+    # XXX: In this test, we are acting as the "remote" peer.
+    # So, technically, we are "remote", and we are sending stuff to "local".
 
-    :gen_tcp.close(lsock)
+      with {:ok, _pid} <- Peer.connect(@remote_address, @local_peer_id, @info_hash),
+           {:ok, sock} <- :gen_tcp.accept(lsock, 5_000),
+           {:ok, handshake_packet} <- :gen_tcp.recv(sock, 68),
+           :ok <- :gen_tcp.send(sock, @remote_handshake),
+           :ok <- :gen_tcp.close(sock)
+      do
+        handshake =
+          handshake_packet
+          |> IO.iodata_to_binary()
+          |> Handshake.decode()
+        assert {:ok, {@local_peer_id, @info_hash, _}} = handshake
+      end
   end
 
 end
