2ea20138cdee9bb26623dec146f9586e64a78c2b
Rosa Richter <robert.c.richter@gmail.com>
2018-02-19 09:46:47 -0600

    Change peer socket to use an active TCP socket

--- lib/effusion/pwp/peer_socket.ex
+++ lib/effusion/pwp/peer_socket.ex
@@ -3,7 +3,6 @@ defmodule Effusion.PWP.PeerSocket do
   alias Effusion.PWP.Messages
 
   def start_link(opts) do
-    parent_process = self()
     GenServer.start_link(__MODULE__, opts)
   end
 
@@ -11,11 +10,15 @@ defmodule Effusion.PWP.PeerSocket do
     {:ok, %{parent_process: parent_process, lsock: lsock}, 0}
   end
 
-  def handle_info(:timeout, %{parent_process: parent_process, lsock: lsock}) do
+  def handle_info(:timeout, %{lsock: lsock} = state) do
     {:ok, sock} = :gen_tcp.accept(lsock)
+    :ok = :inet.setopts(sock, active: true)
+    {:noreply, Map.put(state, :sock, sock)}
+  end
 
-    {:ok, packet} = :gen_tcp.recv(sock, 0)
+  def handle_info({:tcp, _sock, packet}, %{parent_process: parent_process} = state) do
     {:ok, msg} = packet |> IO.iodata_to_binary() |> Messages.decode()
     send parent_process, msg
+    {:noreply, state}
   end
 end
