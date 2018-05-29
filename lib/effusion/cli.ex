cca952dbc13e2bdecd1b094561c8d35cd99317d6
Rosa Richter <rosa.richter@protonmail.com>
2018-05-29 16:12:42 -0500

    Use OptionParser for CLI

--- lib/effusion/cli.ex
+++ lib/effusion/cli.ex
@@ -1,7 +1,14 @@
 defmodule Effusion.CLI do
-  def main(args) do
-    [file, dest] = args
+
+  @aliases [o: :output]
+  @strict [output: :string]
+
+  def main(argv) do
+    {[output: dest], [file], invalid} = OptionParser.parse(argv, strict: @strict, aliases: @aliases)
+    Enum.each(invalid, fn(i) ->
+      IO.warn("Invalid option #{i}")
+    end)
+
     {:ok, _torrent} = Effusion.download(file, dest)
-    IO.puts "done!"
   end
 end
