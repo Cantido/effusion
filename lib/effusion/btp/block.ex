4355921280c6fbe9e2305ce99bbd4c51072726df
Rosa Richter <rosa.richter@protonmail.com>
2018-05-29 14:55:51 -0500

    Make the Block module know how to split blocks

--- lib/effusion/btp/block.ex
+++ lib/effusion/btp/block.ex
@@ -6,6 +6,10 @@ defmodule Effusion.BTP.Block do
     %{index: i, offset: o, data: d}
   end
 
+  def new(i, o, s) when is_index(i) and is_index(o) and is_size(s) do
+    %{index: i, offset: o, size: s}
+  end
+
   def sequential?(%{index: i1, offset: o1, data: d1}, %{index: i2, offset: o2}) do
     (i1 == i2) and (o1 + byte_size(d1) == o2)
   end
@@ -45,5 +49,35 @@ defmodule Effusion.BTP.Block do
 
   def to_piece(%{index: i, offset: _, data: d}) do
     %{index: i, data: d}
+  end
+
+  def split(%{index: index, offset: 0, size: piece_size} = piece, block_size) when is_index(index) and is_size(piece_size) and is_size(block_size) do
+    {whole_block_count, last_block_size} = divrem(piece.size, block_size)
+    whole_block_indices = 0..(whole_block_count - 1)
+
+    whole_blocks =
+      if whole_block_count > 0 do
+        for b_i <- whole_block_indices,
+            offset = b_i * block_size,
+            into: MapSet.new()
+        do
+          new(piece.index, offset, block_size)
+        end
+      else
+        MapSet.new()
+      end
+
+    if last_block_size == 0 do
+      whole_blocks
+    else
+      last_block_index = whole_block_count
+      last_block_offset = last_block_index * block_size
+      last_block = new(piece.index, last_block_offset, last_block_size)
+      MapSet.put(whole_blocks, last_block)
+    end
+  end
+
+  defp divrem(a, b) when is_integer(a) and is_integer(b) and b != 0 do
+    {div(a, b), rem(a, b)}
   end
 end
