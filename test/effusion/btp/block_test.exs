762095a6ceb5d38f7d7d1fa8ca5f10be44e478ec
Rosa Richter <rosa.richter@protonmail.com>
2018-06-02 15:58:00 -0500

    Fill in some tests for Block

--- test/effusion/btp/block_test.exs
+++ test/effusion/btp/block_test.exs
@@ -11,6 +11,10 @@ defmodule Effusion.BTP.BlockTest do
     refute Block.sequential?(
       Block.new(0, 1, "i"),
       Block.new(0, 0, "h"))
+
+    refute Block.sequential?(
+      Block.new(0, 0, "a"),
+      Block.new(1, 1, "d"))
   end
 
   test "adjacent blocks" do
@@ -21,6 +25,10 @@ defmodule Effusion.BTP.BlockTest do
     assert Block.adjacent?(
       %{index: 0, offset: 1, data: "i"},
       %{index: 0, offset: 0, data: "h"})
+
+    refute Block.adjacent?(
+      %{index: 0, offset: 0, data: "a"},
+      %{index: 0, offset: 2, data: "c"})
   end
 
   test "adjacent blocks can be merged" do
@@ -42,5 +50,29 @@ defmodule Effusion.BTP.BlockTest do
 
   test "knows its size if it IDs a block" do
     assert 1 == Block.size(%{index: 0, offset: 0, size: 1})
+  end
+
+  test "an empty block is split into nothing" do
+    unsplit = Block.id(0, 0, 0)
+    splits = Block.split(unsplit, 2)
+
+    assert Enum.count(splits) == 0
+    assert splits == MapSet.new
+  end
+
+  test "can be split into uneven pieces" do
+    unsplit = Block.id(0, 0, 5)
+    splits = Block.split(unsplit, 2)
+
+    assert Enum.count(splits) == 3
+    assert splits == MapSet.new([Block.id(0, 0, 2), Block.id(0, 2, 2), Block.id(0, 4, 1)])
+  end
+
+  test "can be split into even pieces" do
+    unsplit = Block.id(0, 0, 6)
+    splits = Block.split(unsplit, 2)
+
+    assert Enum.count(splits) == 3
+    assert splits == MapSet.new([Block.id(0, 0, 2), Block.id(0, 2, 2), Block.id(0, 4, 2)])
   end
 end
