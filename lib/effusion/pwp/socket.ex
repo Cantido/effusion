05f1503d74dc64f4f3be266de3e13ff4a8c00b11
Rosa Richter <rosa.richter@asynchrony.com>
2018-06-08 11:24:36 -0500

    Make Socket more gracefully handle errors

--- lib/effusion/pwp/socket.ex
+++ lib/effusion/pwp/socket.ex
@@ -3,15 +3,18 @@ defmodule Effusion.PWP.Socket do
   alias Effusion.PWP.Messages
   alias Effusion.BTP.Peer
   def connect(peer) do
-    {host, port} = peer.address
-    {:ok, socket} = :gen_tcp.connect(host, port, [active: false], 1_000)
-
-    :ok = send_msg(socket, Peer.get_handshake(peer))
-    {:ok, hs_bin} = :gen_tcp.recv(socket, 68)
-    {:ok, hs = {:handshake, _, _, _}} = Messages.decode(IO.iodata_to_binary(hs_bin))
-    {:ok, peer} = Peer.handshake(peer, hs)
-    :ok = :inet.setopts(socket, active: true, packet: 4)
-    {:ok, socket, peer}
+    with {host, port} <- peer.address,
+         {:ok, socket} <- :gen_tcp.connect(host, port, [active: false], 1_000),
+         :ok = send_msg(socket, Peer.get_handshake(peer)),
+         {:ok, hs_bin} <- :gen_tcp.recv(socket, 68),
+         {:ok, hs = {:handshake, _, _, _}} <- Messages.decode(IO.iodata_to_binary(hs_bin)),
+         {:ok, peer} <- Peer.handshake(peer, hs),
+         :ok = :inet.setopts(socket, active: true, packet: 4)
+    do
+      {:ok, socket, peer}
+    else
+      err -> err
+    end
   end
 
   def send_msg(socket, msg) do
