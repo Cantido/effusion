fd7e6cef6892637cc7258f7ef1de91f8043be85e
Rosa Richter <rosa.richter@protonmail.com>
2018-06-16 11:05:52 -0500

    Don't try to connect if we don't have any peers.

--- lib/effusion/btp/session.ex
+++ lib/effusion/btp/session.ex
@@ -197,14 +197,20 @@ defmodule Effusion.BTP.Session do
   defp select_peer(s) do
     disconnected_addresses = Enum.map(s.closed_connections, fn c -> c.address end) |> MapSet.new()
     disconnected_ids = Enum.map(s.closed_connections, fn c -> c.remote_peer_id end) |> MapSet.new()
-    s.peers
-       |> Enum.reject(fn(p) ->
-         peer_id = Map.get(p, :remote_peer_id)
-         peer_id == s.peer_id
-         || MapSet.member?(disconnected_addresses, p.address)
-         || MapSet.member?(disconnected_ids, peer_id)
-       end)
-       |> Enum.random()
+
+    eligible_peers = s.peers
+    |> Enum.reject(fn(p) ->
+      peer_id = Map.get(p, :remote_peer_id)
+      peer_id == s.peer_id
+      || MapSet.member?(disconnected_addresses, p.address)
+      || MapSet.member?(disconnected_ids, peer_id)
+    end)
+
+    if Enum.empty?(eligible_peers) do
+      nil
+    else
+      Enum.random(eligible_peers)
+    end
   end
 
   defp connect_to_peer(s, peer) do
