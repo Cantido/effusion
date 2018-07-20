5a58a2aa8639589d2d06ba7d895866d9e45dab54
Rosa Richter <rosa.richter@asynchrony.com>
2018-07-20 14:16:09 -0500

    Create a new module for peer selection

--- lib/effusion/btp/session.ex
+++ lib/effusion/btp/session.ex
@@ -332,24 +332,7 @@ defmodule Effusion.BTP.Session do
   end
 
   defp select_peer(s) do
-    disconnected_addresses = Enum.map(s.closed_connections, fn c -> c.address end) |> MapSet.new()
-    disconnected_ids = Enum.map(s.closed_connections, fn c -> c.remote_peer_id end) |> MapSet.new()
-
-    eligible_peers = s.peers
-    |> Enum.reject(fn(p) ->
-      peer_id = Map.get(p, :remote_peer_id)
-
-      reject_peer_id? = peer_id != nil && ((peer_id == s.peer_id) || MapSet.member?(disconnected_ids, peer_id))
-      reject_address? = MapSet.member?(disconnected_addresses, p.address)
-
-      reject_address? || reject_peer_id?
-    end)
-
-    if Enum.empty?(eligible_peers) do
-      nil
-    else
-      Enum.random(eligible_peers)
-    end
+    Effusion.BTP.PeerSelection.select_peer(s)
   end
 
   defp connect_to_peer(s, peer) do
