1a4c1a2e29953ce3fd364190bac4737066d8a5eb
Rosa Richter <rosa.richter@protonmail.com>
2019-10-27 17:37:36 -0500

    Add Mint 19.2 as a test torrent
    It's about time I updated what torrent I was using to test.

--- mint.exs
+++ mint.exs
@@ -1,4 +1,4 @@
-{:ok, file} = File.read "test/linuxmint-18.3-cinnamon-64bit.iso.torrent"
+{:ok, file} = File.read "test/linuxmint-19.2-cinnamon-64bit.iso.torrent"
 
 {:ok, meta} = Effusion.BTP.Metainfo.decode(file)
 
