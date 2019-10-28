635e01ccb8733e3f67c4736025a27cea830debd8
Rosa Richter <rosa.richter@protonmail.com>
2019-10-27 22:09:00 -0500

    Format the number of bytes downloaded

--- lib/effusion/cli.ex
+++ lib/effusion/cli.ex
@@ -43,10 +43,25 @@ defmodule Effusion.CLI do
     total_to_download = Effusion.BTP.Metainfo.bytes_count(download.meta)
     fraction_downloaded = downloaded / total_to_download
 
+    downloaded_str = format_bytes(downloaded)
+
     IO.puts "NAME \t\t\t\t\t| PERCENT \t| DOWNLOADED \t| DURATION"
-    IO.write "#{download.meta.info.name} \t| #{Float.round(fraction_downloaded * 100, 3)}% \t| #{downloaded}B \t|#{Timex.format_duration dur, :humanized} "
+    IO.write "#{download.meta.info.name} \t| #{Float.round(fraction_downloaded * 100, 3)}% \t| #{downloaded_str} \t| #{Timex.format_duration dur, :humanized} "
 
     Process.sleep(1000)
     output_loop(info_hash)
+  end
+
+  @kibibyte 1024
+  @mebibyte 1024 * @kibibyte
+  @gibibyte 1024 * @mebibyte
+
+  defp format_bytes(n) do
+    cond do
+      n < @kibibyte -> "#{n} B"
+      n < @mebibyte -> "#{Float.round(n/@kibibyte, 1)} kiB"
+      n < @gibibyte -> "#{Float.round(n/@mebibyte, 1)} MiB"
+      true -> "#{Float.round(n/@mebibyte, 1)} GiB"
+    end
   end
 end
