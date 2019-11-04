eb664890abb5e8386d1b1e1cee01a7743c79543d
Rosa Richter <cosmic.lady.rosa@gmail.com>
2019-11-04 15:08:48 -0600

    Select a certain number of peers, not just the highest category

--- lib/effusion/btp/peer_selection.ex
+++ lib/effusion/btp/peer_selection.ex
@@ -1,9 +1,9 @@
 defmodule Effusion.BTP.PeerSelection do
-  def select_peer(peers) do
+  def select_peer(peers, count \\ 50) do
     if Enum.empty?(peers) do
       nil
     else
-      Enum.random(select_lowest_failcount_category(peers))
+      Enum.random(select_lowest_failcount(peers, count))
     end
   end
 
@@ -12,5 +12,12 @@ defmodule Effusion.BTP.PeerSelection do
     |> Enum.chunk_by(&(&1.failcount))
     |> Enum.sort_by(&List.first(&1).failcount)
     |> List.first()
+  end
+
+
+  defp select_lowest_failcount(peers, count) do
+    peers
+    |> Enum.sort_by(&(&1.failcount))
+    |> Enum.take(count)
   end
 end
