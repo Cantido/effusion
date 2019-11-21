121d9a5256fec02402a28b54947aa7efb04a9202
Rosa Richter <cosmic.lady.rosa@gmail.com>
2019-11-21 13:18:05 -0600

    Wrap HTTPoison for THP client

--- lib/effusion/thp/http.ex
+++ lib/effusion/thp/http.ex
@@ -2,7 +2,7 @@ defmodule Effusion.THP.HTTP do
   alias Effusion.Statistics.Net, as: NetStats
   alias Effusion.THP.Decode
   import Effusion.BTP.Peer
-  import Effusion.Hash
+  import Effusion.Hash, only: [is_hash: 1]
   require Logger
   use HTTPoison.Base
   @behaviour Effusion.THP
@@ -45,7 +45,6 @@ defmodule Effusion.THP.HTTP do
              uploaded >= 0 and
              downloaded >= 0 and
              left >= 0 do
-    http_client = Keyword.get(opts, :http_client, HTTPotion)
     sanitized_opts = Keyword.take(opts, @allowed_opts) |> Map.new()
 
     tracker_request = %{
@@ -62,33 +61,45 @@ defmodule Effusion.THP.HTTP do
     Logger.debug("Making announce to #{tracker_url}")
 
     query = URI.encode_query(tracker_request)
-    http_res = http_client.get(tracker_url <> "?" <> query)
-    record_request_stats(query, http_res)
-    Logger.debug("Announce to #{tracker_url} successful.")
-
-    if http_res.status_code == 200 do
-      case ExBencode.decode(http_res.body) do
-        {:ok, bterm} ->
-          tokenized =
-            bterm
-            |> Effusion.Map.rename_keys(@body_names)
-            |> Map.update(:peers, [], &Decode.decode_peers/1)
-
-          {:ok, tokenized}
-
-        err ->
-          err
-      end
-    else
-      {:error, {:http_request_failed, http_res.status_code, http_res}}
+    case get(tracker_url <> "?" <> query) do
+      {:ok, resp} ->
+        Logger.debug("Announce to #{tracker_url} successful.")
+        {:ok, resp.body}
+      {:error, reason} ->
+        Logger.error("Announce to #{tracker_url} failed: #{inspect reason}")
+        {:error, reason}
     end
   end
 
-  def record_request_stats(query, response) do
-    byte_size(query) |> NetStats.add_sent_bytes()
-    byte_size(query) |> NetStats.add_sent_tracker_bytes()
-    {length, ""} = response.headers["content-length"] |> Integer.parse()
+  def process_request_url(url) do
+    byte_size(url) |> NetStats.add_sent_bytes()
+    byte_size(url) |> NetStats.add_sent_tracker_bytes()
+    url
+  end
+
+  @doc """
+  Parses the response body from trackers.
+
+  ## Example
+
+      iex> Effusion.THP.HTTP.process_response_body("d8:intervali1800ee")
+      %{interval: 1800, peers: []}
+  """
+  def process_response_body(body) do
+    case ExBencode.decode(body) do
+      {:ok, bterm} ->
+          bterm
+          |> Effusion.Map.rename_keys(@body_names)
+          |> Map.update(:peers, [], &Decode.decode_peers/1)
+      _err ->
+        raise "tracker response body had bad bencoding"
+    end
+  end
+
+  def process_response_headers(headers) do
+    {length, ""} = headers["content-length"] |> Integer.parse()
     length |> NetStats.add_recv_bytes()
     length |> NetStats.add_recv_tracker_bytes()
+    headers
   end
 end
--- test/effusion/thp/http_test.exs
+++ test/effusion/thp/http_test.exs
@@ -1,47 +1,4 @@
 defmodule Effusion.THP.HTTPTest do
   use ExUnit.Case
-  alias Effusion.THP.HTTP
-  import Mox
   doctest Effusion.THP.HTTP
-
-  @body File.read!("test/tracker_response.txt")
-
-  
-  test "sends STARTED event" do
-    Effusion.HTTP.Mock
-    |> expect(:get, fn uri ->
-
-      [_host, query] = String.split(uri, "?")
-      query = URI.decode_query(query)
-
-      assert query["info_hash"] == "12345678901234567890"
-      assert query["peer_id"] == "12345678901234567890"
-      assert query["port"] == "8001"
-      assert query["uploaded"] == "0"
-      assert query["downloaded"] == "0"
-      assert query["left"] == "0"
-      assert query["event"] == "started"
-      assert query["ip"] == "192.168.1.1"
-      assert query["trackerid"] == "this is my tracker id"
-
-      %{body: @body, status_code: 200, headers: %{"content-length" => (byte_size(@body) |> to_string())}}
-    end)
-
-    {:ok, res} =
-      HTTP.announce(
-        "http://localhost:9000/announce",
-        {192, 168, 1, 1},
-        8001,
-        "12345678901234567890",
-        "12345678901234567890",
-        0,
-        0,
-        0,
-        event: :started,
-        trackerid: "this is my tracker id",
-        http_client: Effusion.HTTP.Mock
-      )
-
-    assert res.interval == 1800
-  end
 end
