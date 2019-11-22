4eb0b4b127390978074abb8f182c833c3355f7f5
Rosa Richter <cosmic.lady.rosa@gmail.com>
2019-11-22 12:32:36 -0600

    Refactor

--- lib/effusion_web/schema.ex
+++ lib/effusion_web/schema.ex
@@ -1,6 +1,6 @@
 defmodule EffusionWeb.Schema do
   use Absinthe.Schema
-  alias EffusionWeb.Resolvers.Torrents
+  alias EffusionWeb.Resolvers
 
   import_types Absinthe.Type.Custom
 
@@ -16,13 +16,13 @@ defmodule EffusionWeb.Schema do
   query do
     @desc "Get all torrents"
     field :torrents, list_of(:torrent) do
-      resolve &Torrents.all_torrents/3
+      resolve &Resolvers.Torrents.all_torrents/3
     end
 
     @desc "Get a torrent"
     field :torrent, :torrent do
       arg :id, non_null(:id)
-      resolve &Torrents.find_torrent/3
+      resolve &Resolvers.Torrents.find_torrent/3
     end
   end
 end
