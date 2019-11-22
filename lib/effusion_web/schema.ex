c5814e9fa99c1ebe1aebe060127e1d79cb30b823
Rosa Richter <cosmic.lady.rosa@gmail.com>
2019-11-21 20:03:20 -0600

    Add descs for queries

--- lib/effusion_web/schema.ex
+++ lib/effusion_web/schema.ex
@@ -2,12 +2,14 @@ defmodule EffusionWeb.Schema do
   use Absinthe.Schema
   alias EffusionWeb.Resolvers.Torrents
 
+  @desc "A torrent"
   object :torrent do
     field :id, :string
     field :name, :string
   end
 
   query do
+    @desc "Get all torrents"
     field :torrents, list_of(:torrent) do
       resolve &Torrents.all_torrents/3
     end
