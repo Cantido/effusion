3faa51319b63c2f17c273654b4789da6cacadf20
Rosa Richter <cosmic.lady.rosa@gmail.com>
2019-12-26 19:39:58 -0600

    Add a distance metric function

--- lib/effusion/dht/dht.ex
+++ lib/effusion/dht/dht.ex
@@ -1,4 +1,6 @@
 defmodule Effusion.DHT do
+  import Bitwise
+
   @moduledoc """
   Documentation for Effusion.DHT.
   """
@@ -17,5 +19,23 @@ defmodule Effusion.DHT do
 
   def token do
     Base.encode64(:crypto.strong_rand_bytes(4))
+  end
+
+  @doc """
+    Calcuates the distance between two node IDs.
+    Smaller values mean the two nodes are closer.
+
+    This function uses the Kademlia distance metric: XOR.
+
+    ## Examples
+
+        iex> Effusion.DHT.distance("12345678901234567890", "12345678901234567890")
+        0
+
+        iex> Effusion.DHT.distance("12345678901234567890", "09876543210987654321")
+        5955258228003349104393039705260020053666630401
+  """
+  def distance(<<a::160>>, <<b::160>>) do
+    bxor(a, b)
   end
 end
