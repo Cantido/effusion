aff5ea2db1e10f30ef694c5ff65c9050c64ad80f
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-05-22 11:42:37 -0600

    Tweak enum alteration

--- priv/repo/migrations/20200113010721_add_dht_state_to_torrents.exs
+++ priv/repo/migrations/20200113010721_add_dht_state_to_torrents.exs
@@ -1,9 +1,23 @@
 defmodule Effusion.Repo.Migrations.AddDhtStateToTorrents do
   use Ecto.Migration
 
+  # we have to dance around with the defaults and with deleting/creating the torrent_status
+  # because altering the enum is not allowed in a transaction.
   def up do
-    execute """
-    alter type torrent_status add value if not exists 'dht_only'
-    """
+    execute "ALTER TABLE torrents ALTER COLUMN state TYPE VARCHAR(255);"
+    execute "ALTER TABLE torrents ALTER COLUMN state SET DEFAULT 'paused';"
+    execute "drop type if exists torrent_status;"
+    execute "create type torrent_status as enum ('paused', 'downloading', 'finished', 'dht_only');"
+    execute "ALTER TABLE torrents ALTER COLUMN state SET DEFAULT 'paused'::torrent_status;"
+    execute "ALTER TABLE torrents ALTER COLUMN state TYPE torrent_status USING (state::torrent_status);"
+  end
+
+  def down do
+    execute "ALTER TABLE torrents ALTER COLUMN state TYPE VARCHAR(255);"
+    execute "ALTER TABLE torrents ALTER COLUMN state SET DEFAULT '';"
+    execute "drop type if exists torrent_status;"
+    execute "create type torrent_status as enum ('paused', 'downloading', 'finished');"
+    execute "ALTER TABLE torrents ALTER COLUMN state SET DEFAULT 'paused'::torrent_status;"
+    execute "ALTER TABLE torrents ALTER COLUMN state TYPE torrent_status USING (state::torrent_status);"
   end
 end
