5f5b4e42c5a180e3b3f1151d79330ff723e6f5d2
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-05-23 18:08:43 -0600

    Make PieceQueue a bounded queue
    For backpressure!

--- lib/effusion/io/piece_queue.ex
+++ lib/effusion/io/piece_queue.ex
@@ -1,12 +1,14 @@
 defmodule Effusion.IO.PieceQueue do
   use GenServer
 
+  @queue_length 100
+
   def start_link(_args) do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
-    {:ok, []}
+    {:ok, %{queue: [], waiting: []}}
   end
 
   def push(msg) do
@@ -17,13 +19,27 @@ defmodule Effusion.IO.PieceQueue do
     GenServer.call(__MODULE__, {:pop, count})
   end
 
-  def handle_call({:push, msg}, _from, state) do
-    {:reply, :ok, [msg | state]}
+  def handle_call({:push, msg}, from, state) do
+    if Enum.count(state.queue) >= @queue_length do
+      waiting = state.waiting ++ [{from, msg}]
+      {:noreply, %{state | waiting: waiting}}
+    else
+      queue = state.queue ++ [msg]
+      {:reply, :ok, %{state | queue: queue}}
+    end
   end
 
   def handle_call({:pop, count}, _from, state) do
-    {popped, remaining} = Enum.split(state, count)
+    {popped, remaining} = Enum.split(state.queue, count)
+    {popped_waiters, still_waiting} = Enum.split(state.waiting, count)
 
-    {:reply, popped, remaining}
+    msgs_from_waiters = Enum.map(popped_waiters, fn {from, msg} ->
+      GenServer.reply(from, :ok)
+      msg
+    end)
+
+    queue = remaining ++ msgs_from_waiters
+
+    {:reply, popped, %{state | queue: queue, waiting: still_waiting}}
   end
 end
