5e38b1f9a6a0ab67c248fc44dfaa5a245637ab2c
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-07 11:25:04 -0600

    add some aliases

--- lib/effusion/cqrs/contexts/peers.ex
+++ lib/effusion/cqrs/contexts/peers.ex
@@ -1,31 +1,37 @@
 defmodule Effusion.CQRS.Contexts.Peers do
+  alias Effusion.CQRS.Application
+  alias Effusion.CQRS.Commands.{
+    AddPeer,
+    HandleHandshake,
+    RemoveConnectedPeer
+  }
   def add(info_hash, peer_id, host, port, from) do
-    %Effusion.CQRS.Commands.AddPeer{
+    %AddPeer{
       info_hash: Effusion.Hash.encode(info_hash),
       peer_id: peer_id,
       host: to_string(:inet.ntoa(host)),
       port: port,
       from: :connection}
-    |> Effusion.CQRS.Application.dispatch()
+    |> Application.dispatch()
   end
 
   def successful_handshake(info_hash, peer_id, host, port, initiated_by) do
-    %Effusion.CQRS.Commands.HandleHandshake{
+    %HandleHandshake{
       info_hash: Effusion.Hash.encode(info_hash),
       peer_id: peer_id,
       host: to_string(:inet.ntoa(host)),
       port: port,
       initiated_by: initiated_by}
-    |> Effusion.CQRS.Application.dispatch()
+    |> Application.dispatch()
   end
 
   def disconnected(info_hash, peer_id, host, port, reason) do
-    %Effusion.CQRS.Commands.RemoveConnectedPeer{
+    %RemoveConnectedPeer{
       info_hash: Effusion.Hash.encode(info_hash),
       peer_id: peer_id,
       host: to_string(:inet.ntoa(host)),
       port: port,
       reason: reason}
-    |> Effusion.CQRS.Application.dispatch()
+    |> Application.dispatch()
   end
 end
