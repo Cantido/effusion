0c0a54074c6b8de64a091c7e849c01d5da7bb327
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-08 07:47:47 -0600

    Migrate IO module

--- lib/effusion/btp/file.ex
+++ lib/effusion/btp/file.ex
@@ -54,44 +54,4 @@ defmodule Effusion.BTP.File do
     join: torrent in assoc(file, :torrent),
     where: torrent.info_hash == ^info_hash
   end
-
-  def split_bytes_to_files(destdir, [], name, piece_length, index, data) do
-    path = Path.join(destdir, name)
-    locbytes = {index * piece_length, data}
-
-    %{path => locbytes}
-  end
-
-  def split_bytes_to_files(destdir, files, name, piece_length, index, data) do
-    piece_range = Range.poslen(index * piece_length, byte_size(data))
-
-    files
-    |> Enum.with_index()
-    |> Enum.map(fn {f, fi} ->
-      file_start = first_byte_index(files, fi)
-      file_range = Range.poslen(file_start, f.size)
-      {f, file_range}
-    end)
-    |> Enum.filter(fn {_f, file_range} ->
-      Range.overlap?(file_range, piece_range)
-    end)
-    |> Enum.map(fn {f, file_range = file_start.._} ->
-      overlap = Range.overlap(file_range, piece_range)
-      poslen = Range.overlap_poslen(file_range, piece_range)
-
-      file_offset.._ = Effusion.Range.shift(overlap, -file_start)
-      file_data = :binary.part(data, poslen)
-
-      {Path.join([destdir, name, f.path]), {file_offset, file_data}}
-    end)
-  end
-
-  defp first_byte_index(_torrent, 0), do: 0
-
-  defp first_byte_index(files, file_index) when file_index > 0 do
-    files
-    |> Enum.slice(0..(file_index - 1))
-    |> Enum.map(& &1.size)
-    |> Enum.sum()
-  end
 end
