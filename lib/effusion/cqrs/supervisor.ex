d1dc64b1c2d5e964fc48bf2ba0facc1c30cbdbf5
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-08 08:00:17 -0600

    Migrate IO module into FileWriter

--- lib/effusion/cqrs/supervisor.ex
+++ lib/effusion/cqrs/supervisor.ex
@@ -12,6 +12,7 @@ defmodule Effusion.CQRS.Supervisor do
       ProcessManagers.DownloadTorrent,
       ProcessManagers.PeerConnection,
       Effusion.CQRS.EventHandlers.DbWriter,
+      Effusion.CQRS.EventHandlers.FileWriter,
       Effusion.CQRS.EventHandlers.TrackerAnnouncer,
       Effusion.CQRS.EventHandlers.PeerMessenger
     ]
---
+++ test/effusion/cqrs/event_handlers/file_writer_test.exs
@@ -0,0 +1,57 @@
+defmodule Effusion.CQRS.EventHandlers.FileWriterTest do
+  use ExUnit.Case
+  alias Effusion.CQRS.EventHandlers.FileWriter
+  alias Effusion.CQRS.Events.{
+    PieceHashSucceeded
+  }
+  doctest Effusion.CQRS.EventHandlers.FileWriter
+
+  setup do
+    meta =
+      TestHelper.tiny_meta()
+      |> Map.update!(:info_hash, &Effusion.Hash.encode/1)
+    %{single_file_meta: meta}
+  end
+
+  setup do
+    {:ok, metabin} = File.read("test/hello_world.torrent")
+    {:ok, multi_file_meta} = Metatorrent.decode(metabin)
+    meta = Map.update!(multi_file_meta, :info_hash, &Effusion.Hash.encode/1)
+
+    %{multi_file_meta: meta}
+  end
+
+  setup do
+    Temp.track!()
+
+    {:ok, file} = Temp.path()
+    old_destination = Application.get_env(:effusion, :download_destination)
+    Application.put_env(:effusion, :download_destination, file)
+
+    on_exit(fn ->
+      Application.put_env(:effusion, :download_destination, old_destination)
+      File.rm_rf(file)
+    end)
+
+    %{destfile: file}
+  end
+
+  test "writes the contents of the torrent out to a file", %{destfile: file, single_file_meta: meta} do
+    :ok = FileWriter.handle(%PieceHashSucceeded{info_hash: meta.info_hash, info: meta.info, index: 0, data: "tin"}, %{})
+    :ok = FileWriter.handle(%PieceHashSucceeded{info_hash: meta.info_hash, info: meta.info, index: 1, data: "y\n"}, %{})
+
+    file = File.read!(Path.join(file, "tiny.txt"))
+
+    assert file == "tiny\n"
+  end
+
+  test "writes the contents of a multi-file torrent out into a directory", %{destfile: file, multi_file_meta: meta} do
+    :ok = FileWriter.handle(%PieceHashSucceeded{info_hash: meta.info_hash, info: meta.info, index: 0, data: "Hello\nworld!\n"}, %{})
+
+    hello = File.read!(Path.join(file, "hello_world/hello.txt"))
+    world = File.read!(Path.join(file, "hello_world/world.txt"))
+
+    assert hello == "Hello\n"
+    assert world == "world!\n"
+  end
+end
