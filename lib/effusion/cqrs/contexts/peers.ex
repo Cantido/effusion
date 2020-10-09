807868c1768f9fc0ae8a44550f92111ac7561329
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-08 20:09:44 -0600

    Make sure peer_uuid is being passed instead of built

--- lib/effusion/cqrs/contexts/peers.ex
+++ lib/effusion/cqrs/contexts/peers.ex
@@ -6,37 +6,30 @@ defmodule Effusion.CQRS.Contexts.Peers do
     HandleHandshake,
     RemoveConnectedPeer
   }
-  def add(info_hash, peer_id, host, port, from) do
-    info_hash = Effusion.Hash.encode(info_hash)
-    host = to_string(:inet.ntoa(host))
+  def add(peer_uuid, info_hash, peer_id, host, port, from) do
     %AddPeer{
-      peer_uuid: "#{info_hash}:#{host}:#{port}",
-      info_hash: info_hash,
+      peer_uuid: peer_uuid,
+      info_hash: Effusion.Hash.encode(info_hash),
       peer_id: peer_id,
-      host: host,
+      host: to_string(:inet.ntoa(host)),
       port: port,
       from: from}
     |> CQRS.dispatch()
   end
 
-  def send_handshake(info_hash, peer_id, host, port, initiated_by) do
-    info_hash = Effusion.Hash.encode(info_hash)
-    host = to_string(:inet.ntoa(host))
-
-    our_peer_id = Application.fetch_env!(:effusion, :peer_id)
-    our_extensions = Application.fetch_env!(:effusion, :enabled_extensions)
+  def send_handshake(peer_uuid, info_hash, peer_id, initiated_by) do
     %SendHandshake{
-      peer_uuid: "#{info_hash}:#{host}:#{port}",
-      info_hash: info_hash,
+      peer_uuid: peer_uuid,
+      info_hash: Effusion.Hash.encode(info_hash),
       peer_id: peer_id,
-      our_peer_id: our_peer_id,
-      our_extensions: our_extensions,
+      our_peer_id: Application.fetch_env!(:effusion, :peer_id),
+      our_extensions: Application.fetch_env!(:effusion, :enabled_extensions),
       initiated_by: initiated_by,
     }
     |> CQRS.dispatch()
   end
 
-  def handle_handshake(info_hash, peer_id, host, port, initiated_by, extensions) do
+  def handle_handshake(peer_uuid, info_hash, peer_id, host, port, initiated_by, extensions) do
     info_hash = Effusion.Hash.encode(info_hash)
     host = to_string(:inet.ntoa(host))
     %HandleHandshake{
@@ -48,12 +41,10 @@ defmodule Effusion.CQRS.Contexts.Peers do
     |> CQRS.dispatch()
   end
 
-  def disconnected(info_hash, peer_id, host, port, reason) do
-    info_hash = Effusion.Hash.encode(info_hash)
-    host = to_string(:inet.ntoa(host))
+  def disconnected(peer_uuid, info_hash, peer_id, reason) do
     %RemoveConnectedPeer{
-      peer_uuid: "#{info_hash}:#{host}:#{port}",
-      info_hash: info_hash,
+      peer_uuid: peer_uuid,
+      info_hash: Effusion.Hash.encode(info_hash),
       peer_id: peer_id,
       reason: reason}
     |> CQRS.dispatch()
