957084a58d8fd09609a58b4da5708091383baaca
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-09 14:05:03 -0600

    Make sure torrent is started before doing peer stuff

--- test/effusion/cqrs/aggregates/peer_test.exs
+++ test/effusion/cqrs/aggregates/peer_test.exs
@@ -96,7 +96,8 @@ defmodule Effusion.CQRS.Aggregates.PeerTest do
 
     test "when we initiated" do
       peer_uuid = UUID.uuid4()
-      info_hash = "5fbd8f01253892288c4e02fad090d90a3107401c"
+      meta = TestHelper.mint_meta()
+      info_hash = Effusion.Hash.encode(TestHelper.mint_info_hash())
       our_peer_id = "Effusion Experiment!"
       our_extensions = [:dht]
       remote_peer_id = "Other peer ID~~~~~~~"
@@ -104,6 +105,19 @@ defmodule Effusion.CQRS.Aggregates.PeerTest do
       host = "127.0.0.1"
       port = 6801
 
+      :ok = CQRS.dispatch(%AddTorrent{
+        announce: meta.announce,
+        announce_list: meta.announce_list,
+        comment: meta.comment,
+        created_by: meta.created_by,
+        creation_date: meta.creation_date,
+        info: meta.info,
+        info_hash: info_hash
+      })
+      :ok = CQRS.dispatch(%StartDownload{
+        info_hash: info_hash,
+        block_size: Application.fetch_env!(:effusion, :block_size)
+      })
       :ok = CQRS.dispatch(%AddPeer{
         peer_uuid: peer_uuid,
         expected_info_hash: info_hash,
