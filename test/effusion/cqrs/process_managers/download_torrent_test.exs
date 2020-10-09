8dbbe47b052b87df1e700d90983539d67393f587
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-09 10:08:24 -0600

    Add a little test

--- test/effusion/cqrs/process_managers/download_torrent_test.exs
+++ test/effusion/cqrs/process_managers/download_torrent_test.exs
@@ -1,5 +1,19 @@
 defmodule Effusion.CQRS.ProcessManagers.DownloadTorrentTest do
   use ExUnit.Case
+  alias Effusion.CQRS.Events.PeerConnected
+  alias Effusion.CQRS.Commands.SendBitfield
+  alias Effusion.CQRS.ProcessManagers.DownloadTorrent
   doctest Effusion.CQRS.ProcessManagers.DownloadTorrent
 
+  test "handles PeerConnected" do
+    peer_uuid = UUID.uuid4()
+    command =
+      DownloadTorrent.handle(
+        %DownloadTorrent{pieces: IntSet.new()},
+        %PeerConnected{peer_uuid: peer_uuid, info_hash: TestHelper.mint_info_hash(), initiated_by: :us}
+      )
+
+    assert command.peer_uuid == peer_uuid
+    assert command.bitfield == <<>>
+  end
 end
