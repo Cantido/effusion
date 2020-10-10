3d80163de536429c1c1bb8e419ec5a692e8e8395
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-10 10:49:59 -0600

    Revert "Cleanup"
    This reverts commit 999a2a765bef33f6b9301530d95909fffcb7b584.

--- lib/effusion/cqrs/event_handlers/peer_messenger.ex
+++ lib/effusion/cqrs/event_handlers/peer_messenger.ex
@@ -4,6 +4,7 @@ defmodule Effusion.CQRS.EventHandlers.PeerMessenger do
     name: __MODULE__
 
   alias Effusion.PWP.TCP.Connection
+  alias Effusion.CQRS.Application, as: CQRS
   alias Effusion.CQRS.Commands.HandleHandshake
   alias Effusion.CQRS.Events.{
     AttemptingToConnect,
