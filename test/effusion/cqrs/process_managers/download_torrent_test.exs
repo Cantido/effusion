6283bdb3007d0073274473213e5db54ce0aa7b1f
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-11 03:27:43 -0600

    add a test for the PeerAdded response

--- test/effusion/cqrs/process_managers/download_torrent_test.exs
+++ test/effusion/cqrs/process_managers/download_torrent_test.exs
@@ -1,10 +1,30 @@
 defmodule Effusion.CQRS.ProcessManagers.DownloadTorrentTest do
   use ExUnit.Case
-  alias Effusion.CQRS.Events.PeerConnected
-  alias Effusion.CQRS.Commands.SendBitfield
   alias Effusion.CQRS.ProcessManagers.DownloadTorrent
-  alias Effusion.CQRS.Commands.SendBitfield
+  alias Effusion.CQRS.Commands.{
+    AttemptToConnect,
+    SendBitfield
+  }
+  alias Effusion.CQRS.Events.{
+    PeerAdded,
+    PeerConnected
+  }
   doctest Effusion.CQRS.ProcessManagers.DownloadTorrent
+
+  describe "handling PeerAdded" do
+    test "issues an AttemptToConnect command if we're not at our limt" do
+      peer_uuid = UUID.uuid4()
+      command =
+        DownloadTorrent.handle(
+          %DownloadTorrent{},
+          %PeerAdded{peer_uuid: peer_uuid, expected_info_hash: "", expected_peer_id: "", host: {}, port: 0, from: "tracker"}
+        )
+
+      assert command == %AttemptToConnect{
+        peer_uuid: peer_uuid
+      }
+    end
+  end
 
   describe "handling PeerConnected" do
     test "Sends empty bitfield when we have no pieces" do
