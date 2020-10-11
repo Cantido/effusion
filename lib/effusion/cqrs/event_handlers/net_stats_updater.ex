852d9c87ca089552def82ef3d7dd374cb2f31309
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-11 00:38:31 -0600

    Actually track received bytes

--- lib/effusion/cqrs/event_handlers/net_stats_updater.ex
+++ lib/effusion/cqrs/event_handlers/net_stats_updater.ex
@@ -6,6 +6,15 @@ defmodule Effusion.CQRS.EventHandlers.NetStatsUpdater do
   alias Effusion.PWP.Messages
   alias Effusion.Statistics.Net, as: NetStats
   alias Effusion.CQRS.Events.{
+    PeerChokedUs,
+    PeerHasBitfield,
+    PeerHasPiece,
+    PeerInterestedInUs,
+    PeerCancelledRequest,
+    PeerSentBlock,
+    PeerSentHandshake,
+    PeerUnchokedUs,
+    PeerUninterestedInUs,
     BitfieldSent,
     BlockRequested,
     InterestedSent,
@@ -14,45 +23,87 @@ defmodule Effusion.CQRS.EventHandlers.NetStatsUpdater do
   }
   require Logger
 
+  def handle(%PeerChokedUs{}, _metadata) do
+    add_recv_bytes(:choke)
+  end
+
+  def handle(%PeerHasBitfield{bitfield: bitfield}, _metadata) do
+    add_recv_bytes({:bitfield, Base.decode16!(bitfield)})
+  end
+
+  def handle(%PeerHasPiece{index: index}, _metadata) do
+    add_recv_bytes({:have, index})
+  end
+
+  def handle(%PeerInterestedInUs{}, _metadata) do
+    add_recv_bytes(:interested)
+  end
+
+  def handle(%PeerCancelledRequest{index: index, offset: offset, size: size}, _metadata) do
+    add_recv_bytes({:cancel, index, offset, size})
+  end
+
+  def handle(%PeerSentBlock{index: index, offset: offset, data: data}, _metadata) do
+    add_recv_bytes({:piece, index, offset, data})
+  end
+
+  def handle(%PeerSentHandshake{info_hash: info_hash, peer_id: peer_id}, _metadata) do
+    add_recv_bytes({:handshake, peer_id, info_hash, []})
+  end
+
+  def handle(%PeerUnchokedUs{}, _metadata) do
+    add_recv_bytes(:unchoke)
+  end
+
+  def handle(%PeerUninterestedInUs{}, _metadata) do
+    add_recv_bytes(:uninterested)
+  end
+
   def handle(
     %BitfieldSent{bitfield: bitfield},
     _metadata
   ) do
     {:ok, bitfield} = Base.decode16(bitfield)
-    add_message_bytes({:bitfield, bitfield})
+    add_sent_bytes({:bitfield, bitfield})
   end
 
   def handle(
     %BlockRequested{index: index, offset: offset, size: size},
     _metadata
   ) do
-    add_message_bytes({:request, index, offset, size})
+    add_sent_bytes({:request, index, offset, size})
   end
 
   def handle(
     %InterestedSent{},
     _metadata
   ) do
-    add_message_bytes(:interested)
+    add_sent_bytes(:interested)
   end
 
   def handle(
     %RequestCancelled{index: index, offset: offset, size: size},
     _metadata
   ) do
-    add_message_bytes({:cancel, index, offset, size})
+    add_sent_bytes({:cancel, index, offset, size})
   end
 
   def handle(
     %SendingHave{index: index},
     _metadata
   ) do
-    add_message_bytes({:have, index})
+    add_sent_bytes({:have, index})
   end
 
-  defp add_message_bytes(message) do
+  defp add_sent_bytes(message) do
     {:ok, msg} = Messages.encode(message)
     NetStats.add_sent_bytes(byte_size(msg))
+    :ok
+  end
+
+  defp add_recv_bytes(message) do
+    {:ok, msg} = Messages.encode(message)
+    NetStats.add_recv_bytes(byte_size(msg))
     :ok
   end
 
