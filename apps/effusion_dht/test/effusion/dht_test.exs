c112700e2cc4227e5a9ed7834083da57e5129a39
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-04-16 14:29:57 -0600

    make sure transaction ID gets set properly

--- apps/effusion_dht/test/effusion/dht_test.exs
+++ apps/effusion_dht/test/effusion/dht_test.exs
@@ -12,18 +12,20 @@ defmodule Effusion.DHTTest do
 
   test "responds to ping", %{port: port} do
     {:ok, socket} = :gen_udp.open(0, [:binary, {:active, false}])
-    :ok = :gen_udp.send(socket, 'localhost', port, ping())
+
+    txid = KRPC.generate_transaction_id()
+    ping =
+      txid
+      |> KRPC.new_query("ping", %{sender_id: DHT.generate_node_id()})
+      |> KRPC.encode!()
+
+    :ok = :gen_udp.send(socket, 'localhost', port, ping)
 
     {:ok, {_host, _port, data}} = :gen_udp.recv(socket, 0, 1_000)
     response = KRPC.decode!(data)
 
+    assert response["t"] == txid
     assert response["y"] == "r"
     assert response["r"] == %{"sender_id" => DHT.local_node_id()}
-  end
-
-  defp ping do
-    KRPC.generate_transaction_id()
-    |> KRPC.new_query("ping", %{sender_id: DHT.generate_node_id()})
-    |> KRPC.encode!()
   end
 end
