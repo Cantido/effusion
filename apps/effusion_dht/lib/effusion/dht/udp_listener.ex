aa0c975298a0a222ec79d9925b4c7d9180fe1be3
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-04-16 21:40:51 -0600

    Keep track of peers that have pinged us

--- apps/effusion_dht/lib/effusion/dht/udp_listener.ex
+++ apps/effusion_dht/lib/effusion/dht/udp_listener.ex
@@ -14,57 +14,86 @@ defmodule Effusion.DHT.UDPListener do
     {:ok, %{port: port}, {:continue, :open_socket}}
   end
 
-  def handle_continue(:open_socket, %{port: port}) do
+  def handle_continue(:open_socket, state = %{port: port}) do
     {:ok, socket} = :gen_udp.open(port, [:binary, {:active, 1}])
-    {:noreply, %{port: port, socket: socket}}
+    state =
+      state
+      |> Map.put(:port, port)
+      |> Map.put(:socket, socket)
+    {:noreply, state}
   end
 
   def handle_info({:udp, socket, ip, port, packet}, state) do
     message = KRPC.decode!(packet)
 
-    case message["q"] do
-      "ping" ->
-        response =
-          message["t"]
-          |> KRPC.new_response(%{sender_id: DHT.local_node_id()})
-          |> KRPC.encode!()
+    state =
+      case message["q"] do
+        "ping" ->
 
-        :ok = :gen_udp.send(socket, ip, port, response)
-      "get_peers" ->
-        response_params = %{
-          sender_id: DHT.local_node_id(),
-          token: DHT.generate_announce_peer_token(),
-          nodes: <<>>
-        }
+          response_params = %{
+            sender_id: DHT.local_node_id(),
+          }
 
-        response =
-          message["t"]
-          |> KRPC.new_response(response_params)
-          |> KRPC.encode!()
+          response =
+            message["t"]
+            |> KRPC.new_response(response_params)
+            |> KRPC.encode!()
 
-        :ok = :gen_udp.send(socket, ip, port, response)
-      "find_node" ->
-        response_params = %{
-          sender_id: DHT.local_node_id(),
-          nodes: <<>>
-        }
+          peer = %{
+            id: message["a"]["sender_id"],
+            ip: ip,
+            port: port
+          }
+          :ok = :gen_udp.send(socket, ip, port, response)
 
-        response =
-          message["t"]
-          |> KRPC.new_response(response_params)
-          |> KRPC.encode!()
+          Map.update(state, :peers, [peer], &([peer | &1]))
+        "get_peers" ->
+          response_params = %{
+            sender_id: DHT.local_node_id(),
+            token: DHT.generate_announce_peer_token(),
+            nodes: <<>>
+          }
 
-        :ok = :gen_udp.send(socket, ip, port, response)
-      "announce_peer" ->
-        response =
-          message["t"]
-          |> KRPC.new_error([203, "Bad token"])
-          |> KRPC.encode!()
+          response =
+            message["t"]
+            |> KRPC.new_response(response_params)
+            |> KRPC.encode!()
 
-        :ok = :gen_udp.send(socket, ip, port, response)
-      _ ->
-        Logger.error("Unrecognized message: #{inspect message}")
-    end
+          :ok = :gen_udp.send(socket, ip, port, response)
+          state
+        "find_node" ->
+          nodes =
+            Map.get(state, :peers, [])
+            |> Enum.map(fn peer ->
+              {ip0, ip1, ip2, ip3} = peer.ip
+              peer.id <> <<ip0, ip1, ip2, ip3>> <> <<peer.port::integer-size(16)>>
+            end)
+            |> Enum.join()
+
+          response_params = %{
+            sender_id: DHT.local_node_id(),
+            nodes: nodes
+          }
+
+          response =
+            message["t"]
+            |> KRPC.new_response(response_params)
+            |> KRPC.encode!()
+
+          :ok = :gen_udp.send(socket, ip, port, response)
+          state
+        "announce_peer" ->
+          response =
+            message["t"]
+            |> KRPC.new_error([203, "Bad token"])
+            |> KRPC.encode!()
+
+          :ok = :gen_udp.send(socket, ip, port, response)
+          state
+        _ ->
+          Logger.error("Unrecognized message: #{inspect message}")
+          state
+      end
 
     {:noreply, state}
   end
--- apps/effusion_dht/test/effusion/dht_test.exs
+++ apps/effusion_dht/test/effusion/dht_test.exs
@@ -2,6 +2,7 @@ defmodule Effusion.DHTTest do
   use ExUnit.Case
   alias Effusion.DHT
   alias Effusion.DHT.KRPC
+  import Bitwise
   doctest Effusion.DHT
 
   setup do
@@ -58,18 +59,51 @@ defmodule Effusion.DHTTest do
     assert response["r"]["nodes"] == <<>>
   end
 
-  test "response to find_node", %{port: port} do
+  test "find_node responds with closest nodes", %{port: port} do
     {:ok, socket} = :gen_udp.open(0, [:binary, {:active, false}])
     on_exit fn ->
       :gen_udp.close(socket)
     end
+
+    target = :crypto.strong_rand_bytes(20)
+
+    # generate 8 peers with node IDs very close to the target
+
+    peers =
+      Enum.map(1..8, fn distance ->
+        id = generate_close_id(target, distance)
+        {:ok, socket} = :gen_udp.open(0, [:binary, {:active, :false}])
+        {:ok, port} = :inet.port(socket)
+
+        %{
+          id: id,
+          socket: socket,
+          port: port
+        }
+      end)
+
+    # have those peers ping the server, so it remembers them
+
+    Enum.each(peers, fn peer ->
+      query =
+        KRPC.generate_transaction_id()
+        |> KRPC.new_query("ping", %{sender_id: peer.id})
+        |> KRPC.encode!()
+
+      :ok = :gen_udp.send(peer.socket, 'localhost', port, query)
+      # read to clear the response from the socket
+      {:ok, {_host, _port, _data}} = :gen_udp.recv(peer.socket, 0, 5_000)
+      :ok = :gen_udp.close(peer.socket)
+    end)
+
+    # Now we find_node to get those close peers
 
     txid = KRPC.generate_transaction_id()
     get_peers =
       txid
       |> KRPC.new_query("find_node", %{
           sender_id: DHT.generate_node_id(),
-          target: :crypto.strong_rand_bytes(20)
+          target: target
         })
       |> KRPC.encode!()
 
@@ -78,10 +112,23 @@ defmodule Effusion.DHTTest do
     {:ok, {_host, _port, data}} = :gen_udp.recv(socket, 0, 5_000)
     response = KRPC.decode!(data)
 
+    compacted_peers =
+      Enum.map(peers, fn peer ->
+        peer.id <> <<127, 0, 0, 1>> <> <<peer.port::integer-size(16)>>
+      end)
+
     assert response["t"] == txid
     assert response["y"] == "r"
     assert response["r"]["sender_id"] == DHT.local_node_id()
-    assert response["r"]["nodes"] == <<>>
+
+    Enum.each(compacted_peers, fn compacted_peer ->
+      assert :binary.match(response["r"]["nodes"], compacted_peer) != :nomatch
+    end)
+  end
+
+  defp generate_close_id(target, distance) do
+    id_int = bxor(:crypto.bytes_to_integer(target), distance)
+    <<id_int::integer-size(160)>>
   end
 
   test "response to announce_peer with a bad token", %{port: port} do
