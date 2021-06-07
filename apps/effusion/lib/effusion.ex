7ecc842f22a638b6a8537b3a1574409315b61c29
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-06-07 16:44:36 -0600

    Delete more stuff

--- apps/effusion/lib/effusion.ex
+++ apps/effusion/lib/effusion.ex
@@ -6,19 +6,4 @@ defmodule Effusion do
   @moduledoc """
   A BitTorrent library.
   """
-
-  @doc """
-  Start asynchronously downloading the torrent described by `meta`,
-  and write it to a file in the `destfile` directory.
-
-  This function returns immediately.
-  """
-  def start_download(meta) when is_map(meta) do
-    Downloads.add(meta)
-    |> Downloads.start()
-  end
-
-  def stop_download(info_hash) when is_hash(info_hash) do
-    Effusion.Downloads.stop(info_hash)
-  end
 end
