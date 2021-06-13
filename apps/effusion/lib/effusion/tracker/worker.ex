7b3cb7615c4de51bc5f70ec286b1b40ddb109dc5
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-06-12 20:09:08 -0600

    Stopped tracker updates shouldn't return peers

--- apps/effusion/lib/effusion/tracker/worker.ex
+++ apps/effusion/lib/effusion/tracker/worker.ex
@@ -9,16 +9,18 @@ defmodule Effusion.Tracker.Worker do
     worker.announce(request)
     |> case do
       {:ok, response} ->
-        Enum.each(response.peers, fn response_peer ->
-          address = {response_peer.ip, response_peer.port}
+        unless request.event == "stoppped" do
+          Enum.each(response.peers, fn response_peer ->
+            address = {response_peer.ip, response_peer.port}
 
-          Swarm.add_peer(response_peer.ip, response_peer.port)
-          if response_peer[:peer_id] do
-            Swarm.set_peer_id(address, response_peer[:peer_id])
-          end
+            Swarm.add_peer(response_peer.ip, response_peer.port)
+            if response_peer[:peer_id] do
+              Swarm.set_peer_id(address, response_peer[:peer_id])
+            end
 
-          ActiveDownload.add_peer(request.info_hash, address)
-        end)
+            ActiveDownload.add_peer(request.info_hash, address)
+          end)
+        end
       err -> Logger.error("tracker returned error: #{inspect err}")
     end
   end
