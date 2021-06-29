4fb5280f2f1eac8a8629d0db687a0230803bce0c
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-06-29 13:47:20 -0600

    Revert "Track download speeds in the connection obj"
    This reverts commit 1a5dc539225b04c1a83cf19793116981a2bdd423.

--- apps/effusion/lib/effusion/connection.ex
+++ apps/effusion/lib/effusion/connection.ex
@@ -5,8 +5,6 @@ defmodule Effusion.Connection do
     info_hash: nil,
     address: nil,
     socket: nil,
-    upload_events: [],
-    download_events: [],
     am_choking: true,
     am_interested: false,
     peer_choking: true,
@@ -55,43 +53,5 @@ defmodule Effusion.Connection do
 
   def set_socket(%__MODULE__{} = conn, socket) do
     %__MODULE__{conn | socket: socket}
-  end
-
-  def download_speed(%__MODULE__{} = conn) do
-    Enum.map(conn.download_events, &elem(&1, 0))
-    |> mean()
-  end
-
-  def add_download_event(%__MODULE__{} = conn, byte_count, now) do
-    dl_events =
-      conn.download_events
-      |> Enum.reject(fn {_byte_count, ts} ->
-        DateTime.diff(ts, now) > 20
-      end)
-
-    dl_events = [{byte_count, now} | dl_events]
-
-    %__MODULE__{conn | download_events: dl_events}
-  end
-
-  def upload_speed(%__MODULE__{} = conn) do
-    Enum.map(conn.upload_events, &elem(&1, 0))
-    |> mean()
-  end
-
-  def add_upload_event(%__MODULE__{} = conn, byte_count, now) do
-    ul_events =
-      conn.upload_events
-      |> Enum.reject(fn {_byte_count, ts} ->
-        DateTime.diff(ts, now) > 20
-      end)
-
-    ul_events = [{byte_count, now} | ul_events]
-
-    %__MODULE__{conn | upload_events: ul_events}
-  end
-
-  defp mean(samples) do
-    Enum.sum(samples) / Enum.count(samples)
   end
 end
