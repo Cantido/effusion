ffb4c8191e56a054e169968042c99502c35b6024
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-07-01 13:28:41 -0600

    Just log to console like a normal app

--- apps/effusion/config/dev.exs
+++ apps/effusion/config/dev.exs
@@ -1,12 +1,5 @@
 use Mix.Config
 
-config :logger,
-  backends: [{LoggerFileBackend, :debug_log}]
-
-config :logger, :debug_log,
-  path: "tmp/debug.log",
-  level: :debug
-
 config :effusion,
   server_host: {127, 0, 0, 1},
   server_port: 4001,
