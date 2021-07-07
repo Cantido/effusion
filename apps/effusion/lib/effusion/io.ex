5126259b18a7487febb6bba597f6ddcaf6973f80
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-07-06 22:19:05 -0600

    More IO refactoring

--- apps/effusion/lib/effusion/io.ex
+++ apps/effusion/lib/effusion/io.ex
@@ -1,13 +1,12 @@
 defmodule Effusion.IO do
   alias Effusion.Range
+  alias Effusion.Metadata
   require Logger
 
   def write_piece(data, index, info) do
-
-    files = Map.get(info, :files, [])
     destdir = Application.fetch_env!(:effusion, :download_destination)
 
-    split_bytes_to_files(files, info.name, info.piece_length, index, data)
+    split_bytes_to_files(info, index, data)
     |> Enum.group_by(fn {path, _} -> path end, fn {_, locbytes} -> locbytes end)
     |> Enum.each(fn {rel_path, locbytes} ->
       path = Path.join(destdir, rel_path)
@@ -22,22 +21,16 @@ defmodule Effusion.IO do
     :ok
   end
 
-  def split_bytes_to_files([], name, piece_length, index, data) do
-    locbytes = {index * piece_length, data}
+  def split_bytes_to_files(info = %Metatorrent.SingleFileInfo{}, index, data) do
+    locbytes = {index * info.piece_length, data}
 
-    %{name => locbytes}
+    %{info.name => locbytes}
   end
 
-  def split_bytes_to_files(files, name, piece_length, index, data) when is_list(files) do
-    piece_range = Range.poslen(index * piece_length, byte_size(data))
+  def split_bytes_to_files(info = %Metatorrent.MultiFileInfo{}, index, data) do
+    piece_range = Range.poslen(index * info.piece_length, byte_size(data))
 
-    files
-    |> Enum.with_index()
-    |> Enum.map(fn {f, fi} ->
-      file_start = first_byte_index(files, fi)
-      file_range = Range.poslen(file_start, f.length)
-      {f, file_range}
-    end)
+    Metadata.byte_mappings(info)
     |> Enum.filter(fn {_f, file_range} ->
       Range.overlap?(file_range, piece_range)
     end)
@@ -48,16 +41,7 @@ defmodule Effusion.IO do
       file_offset.._ = Effusion.Range.shift(overlap, -file_start)
       file_data = :binary.part(data, poslen)
 
-      {Path.join([name, f.path]), {file_offset, file_data}}
+      {Path.join([info.name, f.path]), {file_offset, file_data}}
     end)
-  end
-
-  defp first_byte_index(_torrent, 0), do: 0
-
-  defp first_byte_index(files, file_index) when file_index > 0 do
-    files
-    |> Enum.slice(0..(file_index - 1))
-    |> Enum.map(& &1.length)
-    |> Enum.sum()
   end
 end
--- apps/effusion/lib/effusion/metadata.ex
+++ apps/effusion/lib/effusion/metadata.ex
@@ -1,4 +1,6 @@
 defmodule Effusion.Metadata do
+  alias Effusion.Range
+
   def piece_size(info, index) do
     expected_piece_count = Enum.count(info.pieces)
 
@@ -7,5 +9,24 @@ defmodule Effusion.Metadata do
     else
       info.piece_length
     end
+  end
+
+  def byte_mappings(info) do
+    info.files
+    |> Enum.with_index()
+    |> Enum.map(fn {f, fi} ->
+      file_start = first_byte_index(info, fi)
+      file_range = Range.poslen(file_start, f.length)
+      {f, file_range}
+    end)
+  end
+
+  defp first_byte_index(_info, 0), do: 0
+
+  defp first_byte_index(info, file_index) when file_index > 0 do
+    info.files
+    |> Enum.slice(0..(file_index - 1))
+    |> Enum.map(& &1.length)
+    |> Enum.sum()
   end
 end
