62f8009e2cee778d516f3aafe0e4ad65ce431d4b
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-05-11 19:25:04 -0600

    Update Honeydew

--- apps/effusion/mix.exs
+++ apps/effusion/mix.exs
@@ -37,7 +37,7 @@ defmodule Effusion.Mixfile do
       {:bento, "~> 0.9"},
       {:bypass, "~> 2.1", only: :test},
       {:finch, "~> 0.12.0"},
-      {:honeydew, "~> 1.4.6"},
+      {:honeydew, "~> 1.5"},
       {:int_set, "~> 1.5"},
       {:logger_file_backend, "~> 0.0.10"},
       {:metatorrent, "~> 1.0"},
