df0afff5434c2411c2145cb990830dc84b993e05
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-08-03 11:24:03 -0600

    Remove unused deps

--- apps/effusion/mix.exs
+++ apps/effusion/mix.exs
@@ -43,7 +43,6 @@ defmodule Effusion.Mixfile do
       {:metatorrent, "~> 1.0"},
       {:mox, "~> 1.0", only: :test},
       {:ranch, "~> 1.7"},
-      {:temp, "~> 0.4", only: :test},
       {:solvent, github: "Cantido/solvent"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
