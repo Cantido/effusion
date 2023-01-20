4a244319c4bddc545d873f6f257dc09028ee773c
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-01-19 17:57:33 -0700

    Collapse umbrella

--- mix.exs
+++ mix.exs
@@ -1,34 +1,73 @@
-defmodule EffusionUmbrella.Mixfile do
+defmodule Effusion.Mixfile do
   use Mix.Project
 
   def project do
     [
-      version: "0.1.0",
-      apps_path: "apps",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
-      releases: [
-        effusion: [
-          applications: [
-            effusion: :permanent,
-            effusion_cli: :permanent,
-            effusion_dht: :permanent,
-            effusion_desktop: :permanent
-          ]
-        ]
-      ],
+      app: :effusion,
+      version: "0.2.0",
+      elixir: "~> 1.7",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      description: "A BitTorrent library.",
+      escript: [main_module: Effusion.CLI],
+      package: package(),
       deps: deps(),
-      aliases: aliases()
+      docs: docs(),
+      aliases: aliases(),
+      source_url: "https://github.com/Cantido/effusion",
+      dialyzer: [flags: [:error_handling, :race_conditions, :underspecs]]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["test/support", "lib"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  def application do
+    [
+      extra_applications: [:crypto, :logger, :ranch, :runtime_tools],
+      mod: {Effusion.Application, []}
     ]
   end
 
   defp deps do
-    []
+    [
+      {:bento, "~> 0.9"},
+      {:bypass, "~> 2.1", only: :test},
+      {:finch, "~> 0.14.0"},
+      {:httpoison, "~> 1.8"},
+      {:int_set, "~> 1.5"},
+      {:logger_file_backend, "~> 0.0.10"},
+      {:metatorrent, "~> 1.0"},
+      {:mox, "~> 1.0", only: :test},
+      {:ranch, "~> 1.7"},
+      {:solvent, github: "Cantido/solvent"},
+      {:temp, "~> 0.4", only: :test},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+    ]
   end
 
-  defp aliases do
+  defp package do
     [
-      setup: ["cmd mix setup"]
+      name: :effusion,
+      files: ["lib", "mix.exs"],
+      maintainers: ["Rosa Richter"],
+      licenses: ["GPL v3"],
+      links: %{"Github" => "https://github.com/Cantido/effusion"}
+    ]
+  end
+
+  def docs do
+    [
+      main: "Effusion",
+      source_url: "https://github.com/cantido/effusion"
+    ]
+  end
+
+  def aliases do
+    [
+      setup: ["deps.get"],
     ]
   end
 end
