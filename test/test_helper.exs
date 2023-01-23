acb2233419363804dc5558584bf575a8a55452d9
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-01-22 22:57:40 -0700

    Add back removed test functions

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,8 @@ ExUnit.start(capture_log: true)
 ExUnit.configure(timeout: 10_000)
 
 defmodule TestHelper do
+  alias Effusion.DHT.Node
+
   def mint_info_hash do
     # This value was obtained from a Transmission torrent client, so this
     # value is the known-correct info_hash for the Mint 18.3 64-bit torrent.
@@ -49,5 +51,19 @@ defmodule TestHelper do
       info_hash:
         <<95, 189, 143, 1, 37, 56, 146, 40, 140, 78, 2, 250, 208, 144, 217, 10, 49, 7, 64, 28>>
     }
+  end
+
+  def generate_node do
+    Node.generate_node_id()
+    |> generate_node()
+  end
+
+  def generate_node(id) do
+    %Node{
+      id: id,
+      host: {127, 0, 0, 1},
+      port: Enum.random(1024..65535)
+    }
+    |> Node.response_received_at(~U[2021-04-18T14:11:00Z])
   end
 end
